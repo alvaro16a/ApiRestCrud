95384a72973469ee621dbe14fd9cbb7b2f49a2d9
Alvaro Mena <alvaro16a@gmail.com>
2022-02-17 14:57:38 -0500

    Se creo e inicializo el proyecto con Maven y SpringBoot

---
+++ src/test/java/com/apirestcrud/apirestcrud/ApirestcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestcrud.apirestcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
